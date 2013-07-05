2a38026ea7521981301748937f5a83cdbbe40e02
Todd Wolfson <todd@twolfson.com>
2013-07-05 00:44:25 -0700

    Wrote out basic tests

--- test/trunkata_test.js
+++ test/trunkata_test.js
@@ -1,11 +1,42 @@
-var trunkata = require('../lib/trunkata.js');
+var trunkata = require('../lib/trunkata.js'),
+    expect = require('chai').expect;
 
-describe('trunkata', function () {
+describe('A short string <div>', function () {
   before(function () {
+    // Create our content
+    var input = 'abc',
+        node = document.createElement('div');
+    node.innerHTML = input;
 
+    // Save for later
+    this.input = input;
+    this.node = node;
   });
 
-  it('', function () {
+  describe('when truncated', function () {
+    before(function () {
+      trunkata(this.node);
+    });
 
+    it('is unchanged', function () {
+      expect(this.node.innerHTML).to.equal(this.input);
+    });
+  });
+});
+
+describe.skip('A long string <div>', function () {
+  describe('when truncated', function () {
+    before(function () {
+      trunkata(this.node);
+    });
+
+    it('is truncated', function () {
+      expect(this.node.innerHTML).to.equal(this.input);
+    });
+
+    // TODO: More appropriately test event bindings of child nodes and parent node
+    it('is the same node', function () {
+      // TODO: Capture child nodes of div?
+    });
   });
 });
