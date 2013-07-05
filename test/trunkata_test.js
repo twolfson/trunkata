38cc9958cad4556229beea2a903445900f362668
Todd Wolfson <todd@twolfson.com>
2013-07-05 03:04:37 -0700

    Moved to flat file testing =D

--- test/trunkata_test.js
+++ test/trunkata_test.js
@@ -1,13 +1,13 @@
 var trunkata = require('../lib/trunkata.js'),
-    expect = require('chai').expect;
+    expect = require('chai').expect,
+    fs = require('fs'),
+    domify = require('domify');
 
 describe('A short string <div>', function () {
   before(function () {
     // Create our content
-    var input = 'abc',
-        node = document.createElement('div');
-    node.className = 'test-div';
-    node.innerHTML = input;
+    var input = fs.readFileSync(__dirname + '/test_files/short_string.html', 'utf8'),
+        node = domify('<div class="test-div">' + input + '</div>');
     document.body.appendChild(node);
 
     // Save for later
@@ -32,10 +32,8 @@ describe('A short string <div>', function () {
 describe('A long string <div>', function () {
   before(function () {
     // Create our content
-    var input = 'Lorem ipsum Non amet nulla amet deserunt commodo quis Ut cillum occaecat aliqua eu laboris ex in et quis laboris proident dolor pariatur aute ullamco laboris adipisicing reprehenderit sed non enim occaecat dolor aute id non quis dolor consequat velit exercitation mollit nostrud sint sunt occaecat sunt elit tempor ex aute aliqua tempor cupidatat sed esse dolore ea incididunt nulla in sint ad deserunt veniam veniam nisi deserunt ex dolore consequat ut exercitation pariatur exercitation aliquip aliquip eu do occaecat nisi in est fugiat sint qui pariatur dolore laboris cupidatat cupidatat eiusmod aliqua fugiat in dolor sed in pariatur cupidatat do esse dolor in Ut Ut nisi proident anim nisi in eiusmod cillum fugiat labore cillum velit veniam mollit sit aute commodo dolor ut aute exercitation cupidatat occaecat nostrud aute Ut in magna nisi labore officia eu consectetur labore labore dolor in consectetur occaecat quis incididunt sed Duis irure adipisicing proident officia commodo dolore deserunt id in elit amet exercitation Excepteur Ut magna officia aute do ut voluptate Excepteur aliquip proident incididunt amet sit id enim.',
-        node = document.createElement('div');
-    node.className = 'test-div';
-    node.innerHTML = input;
+    var input = fs.readFileSync(__dirname + '/test_files/long_string.html', 'utf8'),
+        node = domify('<div class="test-div">' + input + '</div>');
     document.body.appendChild(node);
 
     // Save for later
